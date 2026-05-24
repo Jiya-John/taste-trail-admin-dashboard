fbd475f028edb7e59d7216bd8690ca6ae4ec065b
Jiya John <jiyajohn015@gmail.com>
2026-05-23 20:27:15 -0400

    Move User routes to manageuser.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { ObjectId } from "mongodb";
 import { getUsers, getSingleUser, addUser, editUser, deleteUser, validateUser } from "../db/users.js";
+import manageUsers from "./manageusers.js";
 
 export default function adminRoutes(db, upload) {
   const router = express.Router();
@@ -47,68 +48,7 @@ export default function adminRoutes(db, upload) {
     res.render("admin/dashboard", { title: "Dashboard", userCount, postCount });
   });
 
-  // Manage Users
-  router.get("/users", requireAdmin, async (req, res) => {
-    const users = await getUsers(db);
-    res.render("admin/users/list", { title: "Manage Users", users });
-  });
-
-  // Add User
-  router.get("/users/add", requireAdmin, (req, res) => {
-    res.render("admin/users/form", {
-      title: "Add User",
-      user: {},
-      formAction: "/admin/users/add",
-    });
-  });
-
-  // Form to add user - Post to db
-  router.post("/users/add", requireAdmin, async (req, res) => {
-    const error = await validateUser(db, req.body, false);
-    if (error) {
-      return res.render("admin/users/form", {
-        title: "Add User",
-        error,
-        formAction: "/admin/users/add",
-        user: req.body,
-      });
-    }
-    await addUser(db, req.body);
-    res.redirect("/admin/users");
-  });
-
-  // Edit User
-  router.get("/users/edit", requireAdmin, async (req, res) => {
-    if (!req.query.id) return res.redirect("/admin/users");
-    const user = await getSingleUser(db, req.query.id);
-    res.render("admin/users/form", {
-      title: "Edit User",
-      user,
-      formAction: "/admin/users/edit",
-    });
-  });
-
-  // Form to edit user - Post to db
-  router.post("/users/edit", requireAdmin, async (req, res) => {
-    const error = await validateUser(db, req.body, true, req.body.id);
-    if (error) {
-      const user = await getSingleUser(db, req.body.id);
-      return res.render("admin/users/form", {
-        title: "Edit User",
-        error,
-        formAction: "/admin/users/edit",
-        user: { ...user, ...req.body },
-      });
-    }
-    await editUser(db, req.body.id, req.body);
-    res.redirect("/admin/users");
-  });
-
-  // Delete user
-  router.get("/users/delete", requireAdmin, async (req, res) => {
-    if (req.query.id) await deleteUser(db, req.query.id);
-    res.redirect("/admin/users");
-  });
+  router.use("/users", requireAdmin, manageUsers(db, requireAdmin));
 
   return router;
 }
---
+++ routes/manageusers.js
@@ -0,0 +1,72 @@
+import express from "express";
+import { ObjectId } from "mongodb";
+import { getUsers, getSingleUser, addUser, editUser, deleteUser, validateUser } from "../db/users.js";
+
+export default function manageUsers(db, requireAdmin) {
+  const router = express.Router();
+
+  // Manage Users
+  router.get("/", requireAdmin, async (req, res) => {
+    const users = await getUsers(db);
+    res.render("admin/users/list", { title: "Manage Users", users });
+  });
+
+  // Add User
+  router.get("/add", requireAdmin, (req, res) => {
+    res.render("admin/users/form", {
+      title: "Add User",
+      user: {},
+      formAction: "/admin/users/add",
+    });
+  });
+
+  // Form to add user - Post to db
+  router.post("/add", requireAdmin, async (req, res) => {
+    const error = await validateUser(db, req.body, false);
+    if (error) {
+      return res.render("admin/users/form", {
+        title: "Add User",
+        error,
+        formAction: "/admin/users/add",
+        user: req.body,
+      });
+    }
+    await addUser(db, req.body);
+    res.redirect("/admin/users");
+  });
+
+  // Edit User
+  router.get("/edit", requireAdmin, async (req, res) => {
+    if (!req.query.id) return res.redirect("/admin/users");
+    const user = await getSingleUser(db, req.query.id);
+    res.render("admin/users/form", {
+      title: "Edit User",
+      user,
+      formAction: "/admin/users/edit",
+    });
+  });
+
+  // Form to edit user - Post to db
+  router.post("/edit", requireAdmin, async (req, res) => {
+    const error = await validateUser(db, req.body, true, req.body.id);
+    if (error) {
+      const user = await getSingleUser(db, req.body.id);
+      return res.render("admin/users/form", {
+        title: "Edit User",
+        error,
+        formAction: "/admin/users/edit",
+        user: { ...user, ...req.body },
+      });
+    }
+    await editUser(db, req.body.id, req.body);
+    res.redirect("/admin/users");
+  });
+
+  // Delete user
+  router.get("/delete", requireAdmin, async (req, res) => {
+    if (req.query.id) await deleteUser(db, req.query.id);
+    res.redirect("/admin/users");
+  });
+
+    return router;
+} 
